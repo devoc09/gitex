8c8779fb65fb8ac680d6f4b2e3816b2f7604153f
Takumi Katase <takumi.katase@devoc.ninja>
2025-11-03 21:29:53 +0900

    chore: can use libvaxis
    Signed-off-by: Takumi Katase <takumi.katase@devoc.ninja>

--- build.zig
+++ build.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{ .name = "gitex", .root_module = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = b.graph.host,
     }) });
+
+    const vaxis = b.dependency("vaxis", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.root_module.addImport("vaxis", vaxis.module("vaxis"));
 
     exe.linkSystemLibrary("git2");
     exe.linkLibC();
